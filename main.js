2d3a3e81a6d9a93c34e0c53a4e079a2dce032d8d
jpeters <jerryongit>
2018-10-11 15:21:54 -0500

    update with Tsolmons code

--- main.js
+++ main.js
@@ -1,126 +1,225 @@
-var copyEmailBtn = document.querySelector('.js-emailcopybtn');
-let myData = [];
-let openRow = "<div class='row'>";
-let closeRow = "</div><br/>"
-copyEmailBtn.addEventListener('click', function(event) {
-  // Select the email link anchor text
-  var emailLink = document.querySelector('.js-emaillink');
-  var range = document.createRange();
-  range.selectNode(emailLink);
-  window.getSelection().addRange(range);
 
-  try {
-    // Now that we've selected the anchor text, execute the copy command
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    alert('Copying URL was ' + msg);
-  } catch(err) {
-    alert('Oops, unable to copy');
+(function () {
+  'use strict';
+
+  var myData = [];
+
+  function init() {
+
+    loadXMLDoc();
+
+    $(document).ready(function () {
+      $(".modal-footer button").on("click", function () {
+        var copyText = document.getElementById("urlholder");
+        copyText.select();
+        document.execCommand("copy");
+      });
+
+      $('.imagepreview').on("click", function (evt) {
+
+        // var copyText = document.getElementById("urlholder");
+        // copyText.select();
+        // document.execCommand("copy");
+      });
+    });
+
   }
 
-  // Remove the selections - NOTE: Should use
-  // removeRange(range) when it is supported
-  window.getSelection().removeAllRanges();
-});
 
 
-loadXMLDoc();
-function loadXMLDoc() {
+
+  function loadXMLDoc() {
     var xmlhttp = new XMLHttpRequest();
 
-    xmlhttp.onreadystatechange = function() {
-        if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
-           if (xmlhttp.status == 200) {
-              myData = JSON.parse(xmlhttp.responseText);
-               document.getElementById("myDiv").innerHTML = displayData(myData);
-           }
-           else if (xmlhttp.status == 400) {
-              alert('There was an error 400');
-           }
-           else {
-               alert('something else other than 200 was returned');
-           }
+    xmlhttp.onreadystatechange = function () {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {   // XMLHttpRequest.DONE == 4
+        if (xmlhttp.status == 200) {
+          myData = JSON.parse(xmlhttp.responseText);
+          buildLeftNav();
+          // document.getElementById("myDiv").innerHTML = displayData(myData);
         }
+        else if (xmlhttp.status == 400) {
+          alert('There was an error 400');
+        }
+        else {
+          alert('something else other than 200 was returned');
+        }
+      }
     };
 
-    xmlhttp.open("GET", "data.json", true);
+    xmlhttp.open("GET", "data2.json", true);
     xmlhttp.send();
-}
+  }
 
-$(document).ready(function() {
-  $(".pop").on("click", function() {
-     $('.imagepreview').attr('src', $('#imageresource').attr('src')); // here asign the image to the modal when the user click the enlarge link
-     $('.js-emaillink').html($('#imageresource').attr('src'));
-     $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-  });
-  $("#copyClipboard").on("click", function() {
-      var copyText = $('#imageURL').attr('src');
-      copyText.select();
-      document.execCommand("copy");
-    //  alert($('#imageURL').attr('src'));
-      alert("Copied the text: " + copyText.value);
-      // copyText.select();
-      // document.execCommand("copy");
-      // alert("Copied the text: " + copyText.value);
-  });
-});
 
-function viewImage(imageURL){
-     $('.imagepreview').attr('src',imageURL); // here asign the image to the modal when the user click the enlarge link
-     $('.js-emaillink').html(imageURL);
-     $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
-}
+  function buildLeftNav() {
 
-function goBack(){
-  location.reload();
-}
+    var img = document.createElement('img');
+    img.src = myData.Profile.Files[0].thumbURL200;
+    $('.left-menu .thumb').append(img);
+    $('.left-menu > .name')[0].innerText = myData.Profile.fullName;
 
-function gotoFolder(folderId){
-  let folderData;
-  for(let folder of myData.Folders){
-      if(folder.archiveNbr==folderId){
-        folderData = folder;
-        break;
-      }
-   }
-   document.getElementById("myDiv").innerHTML = "<input type='button' value='BACK' onclick='goBack();' /><br/>" + getHTMLFromFile(folderData.Files);
-}
-
-function getHTMLFromFile(files){
-  let columnCount =0;
-  let responseText = "<br/><br/>Files:<br/>";
-    for(let file of files){
-      let fileIcon = '<a href="#" onclick=viewImage("'+file.thumbURL200+'");><img src="' + file.thumbURL200 +'"/>'+ file.displayName+'</a>';
-      let displayName = '<div class="col-md-2">'+ fileIcon + '</div>&nbsp;&nbsp;';
-      responseText+=displayName;
-      columnCount++;
-     if(columnCount==6){
-       columnCount =0;
-       responseText+=closeRow;
-       responseText+=openRow;
-     }
+    if (myData.Folders.Folders) {
+      buildTree(myData.Folders.Folders);
     }
 
-    return responseText;
-}
-function displayData(jsonData){
-  let folders = jsonData.Folders;
-  let files = jsonData.Files;
-  let columnCount =0;
-  let responseText="<br/>Folders:<br/><br/>";
-
-  responseText+=openRow;
-  for(let folder of folders){
-    let folderIcon = '<button type="button" class="btn btn-default" aria-label="Left Align" onclick=gotoFolder("'+ folder.archiveNbr+'");><span class="glyphicon glyphicon-folder-close" aria-hidden="true">'+ folder.displayName +'</span></button>';
-    let displayName = "<div class='col-md-2'>"+ folderIcon + "</div>";
-    responseText+=displayName;
-    columnCount++;
-   if(columnCount==6){
-     columnCount =0;
-     responseText+=closeRow;
-     responseText+=openRow;
-   }
   }
-  responseText += getHTMLFromFile(files);
-  return responseText;
-}
+
+  function buildTree(folders, parentArchNbr) {
+    if (folders) {
+      folders.forEach(function (f) {
+        var li = document.createElement('li');
+        var icon = document.createElement('i');
+        var span = document.createElement('span');
+
+        span.innerText = f.displayName;
+        icon.classList.add('fa');
+        icon.classList.add('fa-2x');
+        li.setAttribute('data-archnbr', f.archiveNbr);
+
+
+        if (f.Folders && f.Folders.length > 0) {
+          icon.classList.add('fa-caret-right');
+          li.classList.add('c-pointer');
+          li.classList.add('top-lvl');
+          li.appendChild(icon);
+          li.appendChild(span);
+          var ul = document.createElement('UL');
+          ul.setAttribute('data-archnbr', f.archiveNbr);
+          ul.classList.add('closed');
+          ul.classList.add('submenu');
+          li.addEventListener('click', OnClick);
+          li.appendChild(ul);
+        }
+        else if(!parentArchNbr && f.Folders && f.Folders.length == 0){
+          icon.classList.add('fa-caret-right');
+          li.classList.add('c-pointer');
+          li.classList.add('top-lvl');
+          li.appendChild(icon);
+          li.appendChild(span);
+        }
+        else {
+          li.appendChild(span);
+        }
+
+        li.addEventListener('click', OnClick, { useCapture: true });
+
+        if (parentArchNbr) {
+          li.setAttribute('data-parent', parentArchNbr);
+          $('.left-menu ul[data-archnbr="' + parentArchNbr + '"]').append(li);
+        }
+        else {
+          $('.left-menu ul').append(li);
+        }
+
+        if (f.Folders && f.Folders.length > 0) {
+          buildTree(f.Folders, f.archiveNbr);
+        }
+      });
+    }
+  }
+
+  function OnClick(evt) {
+
+    evt.stopPropagation();
+    var archNbr = evt.currentTarget.getAttribute('data-archnbr');
+
+    if (evt.currentTarget.classList.contains('top-lvl')) {
+      if (evt.currentTarget.classList.contains('opened')) {
+        evt.currentTarget.classList.remove('opened');
+        $(evt.currentTarget).children('ul').removeClass('open');
+        $(evt.currentTarget).children('ul').addClass('closed');
+      }
+      else {
+        evt.currentTarget.classList.add('opened');
+        $(evt.currentTarget).children('ul').addClass('open');
+        $(evt.currentTarget).children('ul').removeClass('closed');
+      }
+    }
+    else {
+      var n = evt.currentTarget.getAttribute('data-archnbr');
+    }
+    loadFolder(archNbr);
+
+  }
+
+  function loadFolder(archNbr) {
+    $('.main-body').empty();
+    var res = fetchChild(myData, { key: 'archiveNbr', val: archNbr }).then(function (res) {
+
+      var header = document.createElement('div');
+      header.classList.add('h1');
+      header.innerText = res.displayName;
+      $('.main-body').append(header);
+
+      if (res.Files) {
+        var fileContainer = document.createElement('div');
+        fileContainer.classList.add('file-list');
+
+        res.Files.forEach(function (f) {
+          var file = document.createElement('div');
+          file.classList.add('file');
+          file.setAttribute('data-archnbr', f.archiveNbr);
+          file.style.backgroundImage = 'url(' + f.thumbURL200 + ')';
+
+          // var name = document.createElement('span');
+          // name.innerText = f.displayName;
+          // file.appendChild(name);
+
+          file.addEventListener('click', OnFileClick, { useCapture: true });
+          fileContainer.appendChild(file);
+
+        });
+        $('.main-body').append(fileContainer);
+      }
+
+
+    });
+
+
+
+  }
+
+  function fetchChild(node, qry) {
+    return new Promise(function (resolve, reject) {
+      if (node[qry.key] == qry.val) {
+        resolve(node);
+        return;
+      }
+      var keys = Object.keys(node);
+      for (var i = 0; i < keys.length; i++) {
+        if (typeof node[keys[i]] == 'object') {
+          fetchChild(node[keys[i]], qry).then(function (res) {
+            resolve(res);
+          });
+        }
+      }
+    });
+  }
+
+  function OnFileClick(evt) {
+    evt.stopPropagation();
+    var archNbr = evt.currentTarget.getAttribute('data-archnbr');
+    var res = fetchChild(myData, { key: 'archiveNbr', val: archNbr }).then(function (res) {
+      $('#myModalLabel').text(res.displayName);
+      $('#urlholder').val(res.fileURL);
+      viewImage(res);
+    });
+
+  }
+
+
+  function viewImage(file) {
+  //res.thumbURL200
+    $('.imagepreview').attr('src', file.thumbURL200); // here asign the image to the modal when the user click the enlarge link
+    $('#fileanchor').attr('href', file.thumbURL2000);
+    
+    // $('.imagepreview').attr('src', imageURL); 
+    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+  }
+
+
+  init();
+
+
+})();
